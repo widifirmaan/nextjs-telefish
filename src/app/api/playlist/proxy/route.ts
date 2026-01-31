79398040dee64858c813e7d1da0c1f1c4d4f858c
Widi Firmansyah <vejoyf@gmail.com>
2026-01-31 22:05:16 +0700

    fix runtime

--- src/app/api/playlist/proxy/route.ts
+++ src/app/api/playlist/proxy/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+export const runtime = 'edge';
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: NextRequest) {
